159b57768982dd34b698cb61e10818e5161bd83e
20191663 <ihs20191663@gmail.com>
2022-09-21 13:37:12 +0900

    functions_4

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,5 +1,17 @@
 import java.util.*    // required import
 
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
+
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
 fun fishFood (day : String) : String {
     return when (day) {
         "Monday" -> "flakes"
@@ -11,17 +23,24 @@ fun fishFood (day : String) : String {
     }
 }
 
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
+}
+
 fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
-
     println ("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
 }
 
-fun randomDay() : String {
-    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else  -> false
+    }
 }
 
 fun main(args: Array<String>) {
